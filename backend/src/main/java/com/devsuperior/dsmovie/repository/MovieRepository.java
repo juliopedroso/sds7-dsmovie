23fb1761fed39a139850304a5d55b98bac0c13d0
SnoopyBR <juliopedroso@gmail.com>
2022-03-11 22:48:27 -0300

    Find movies

---
+++ backend/src/main/java/com/devsuperior/dsmovie/repository/MovieRepository.java
@@ -0,0 +1,12 @@
+package com.devsuperior.dsmovie.repository;
+
+import com.devsuperior.dsmovie.entities.Movie;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie,Long>{
+
+
+}
