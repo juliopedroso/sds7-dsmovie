7884df1572c1e80b590d9b1164b9066544d62f8b
SnoopyBR <juliopedroso@gmail.com>
2022-03-13 11:35:57 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -29,10 +29,15 @@ function Listing() {
             });
     }, [pageNumber])
 
+
+    const handlePageChange = (newPageNumber : number) => {
+        setPageNumber(newPageNumber);
+    }
+
     return (
         <>
             <p>{pageNumber}</p>
-            <Pagination />
+            <Pagination page={page} onChange={handlePageChange} />
             <div className="container">
                 <div className="row">
                     {page.content.map(movie => (
